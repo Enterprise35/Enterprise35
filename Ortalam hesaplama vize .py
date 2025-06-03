a76f7ceea5f01d17d6b00be9ad95693baedf75c5
Enterprise35 <106994209+Enterprise35@users.noreply.github.com>
2025-06-03 22:48:32 +0300

    Update Ortalam hesaplama vize .py

--- Ortalam hesaplama vize .py
+++ Ortalam hesaplama vize .py
@@ -1,14 +1,23 @@
+A =input("İmport your firs midterm:")
+B =input("İmport your second midterm:")
+C =input("İmport your final exam:")
+float(A)
+float(B)
+float(C)
 A = float(input("Import your first midterm: "))
 B = float(input("Import your second midterm: "))
 C = float(input("Import your final exam: "))
 
+A = A*60/100
+B = B*60/100
+C = C*40/100
+␍␊
 A = A*30/100
 B = B*30/100
-C = C*40/100
+C = C*40/100␊
 
 result = A+B+C
 if result >= 50:
     print("Geçti")
 else:
     print("Kaldı")
-    
